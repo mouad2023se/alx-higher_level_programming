aeaf12c5dbad4741c4d7c25b4902ae8ed9115c14
mouad2023se <elharrakmouad@gmail.com>
2023-12-04 13:23:27 -0800

    task13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,114 +1,48 @@
 #include "lists.h"
-
+#include <stdlib.h>
 /**
-* reverse - reverses the second half of the list
-*
-* @h_r: head of the second half
-* Return: no return
-*/
-void reverse(listint_t **h_r)
+ * is_palindrome - check if linked list is palindrome
+ * @head: the adress to the linked list
+ * Return: 1 if true and 0 if not
+ */
+int is_palindrome(listint_t **head)
 {
-	listint_t *prv;
-	listint_t *crr;
-	listint_t *nxt;
+	int n, i, a, li;
+	int *name;
+	listint_t *current;
+	listint_t  *new;
 
-	prv = NULL;
-	crr = *h_r;
-
-	while (crr != NULL)
-	{
-	nxt = crr->next;
-	crr->next = prv;
-	prv = crr;
-	crr = nxt;
-	}
-
-	*h_r = prv;
-}
-
-/**
-* compare - compares each int of the list
-*
-* @h1: head of the first half
-* @h2: head of the second half
-* Return: 1 if are equals, 0 if not
-*/
-int compare(listint_t *h1, listint_t *h2)
-{
-	listint_t *tmp1;
-	listint_t *tmp2;
-
-	tmp1 = h1;
-	tmp2 = h2;
-
-	while (tmp1 != NULL && tmp2 != NULL)
-	{
-	if (tmp1->n == tmp2->n)
-	{
-		tmp1 = tmp1->next;
-		tmp2 = tmp2->next;
-	}
-	else
-	{
-		return (0);
-	}
-	}
-
-	if (tmp1 == NULL && tmp2 == NULL)
+	current = *head;
+	if (*head == NULL)
 	{
 		return (1);
 	}
-
-	return (0);
-}
-
-/**
-* is_palindrome - checks if a singly linked list
-* is a palindrome
-* @head: pointer to head of list
-* Return: 0 if it is not a palindrome,
-* 1 if it is a palndrome
-*/
-int is_palindrome(listint_t **head)
-{
-	listint_t *slow, *fast, *prev_slow;
-	listint_t *scn_half, *middle;
-	int isp;
-
-	slow = fast = prev_slow = *head;
-	middle = NULL;
-	isp = 1;
-
-	if (*head != NULL && (*head)->next != NULL)
+	n = 0;
+	while (current != NULL)
 	{
-	while (fast != NULL && fast->next != NULL)
+		current = current->next;
+		n++;
+	}
+	name = malloc(sizeof(int) * n);
+	if (name == NULL)
 	{
-		fast = fast->next->next;
-		prev_slow = slow;
-		slow = slow->next;
+		return (0);
 	}
-
-	if (fast != NULL)
+	new = *head;
+	for (i = 0; new != NULL && i < n; i++)
 	{
-		 middle = slow;
-	slow = slow->next;
+		name[i] = new->n;
+		new = new->next;
 	}
-
-	scn_half = slow;
-	prev_slow->next = NULL;
-	reverse(&scn_half);
-	isp = compare(*head, scn_half);
-
-	if (middle != NULL)
+	for (a = 0; a < n/2; a++)
 	{
-		prev_slow->next = middle;
-		middle->next = scn_half;
+		li = n - a - 1;
+		if (name[a] != name[li])
+		{
+			free(name);
+			return (0);
+		}
 	}
-	else
-	{
-		prev_slow->next = scn_half;
-	}
-	}
-
-	return (isp);
+	free(name);
+	return (1);
 }
